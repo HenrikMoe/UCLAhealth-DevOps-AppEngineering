7e959ec733c21013c0ba5556848c92e70560dc58
Henrik <54423408+HenrikMoe@users.noreply.github.com>
2024-05-20 15:48:06 -0700

    Update output.tf

--- TerraformAssignment/ParentModule/output.tf
+++ TerraformAssignment/ParentModule/output.tf
@@ -3,7 +3,7 @@
 
 output "resource_names" {
 
-  # extract keys and values from map and repack into child interface
+  # extract keys and values from main for interfacing to child module 
 
   value = { for k, v in module.resource_names : k => v.resource_name }
 
